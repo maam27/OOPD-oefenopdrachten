cd9689d0a638ebe4c0117c7673c8f6764e4a990c
maam27 <martkools@live.nl>
2018-02-27 14:46:17 +0100

    .+,=;

--- eclipse/lottoGame/src/lottoGame/Main.java
+++ eclipse/lottoGame/src/lottoGame/Main.java
@@ -3,9 +3,7 @@ package lottoGame;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Lottomachine lotto = new Lottomachine();
-		lotto.voerTrekkingUit();
+		 new Lottomachine().voerTrekkingUit();		
 	}
 
 }
